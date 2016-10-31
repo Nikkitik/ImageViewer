40607fc551b2ff0892b6254d79fdecb755a1e17c
nikkitik <nikkitik21@gmail.com>
2016-10-31 17:14:25 +0300

    добавлен NavigationView

--- app/src/main/java/com/example/nromantsov/imageviewer/MainActivity.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.nromantsov.imageviewer;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -19,10 +23,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     FragmentManager fragmentManager;
     ViewPager viewPager;
-    List<String> listItem = new ArrayList<>();
     String tagEng = null;
 
     @Override
@@ -30,18 +33,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listItem.add("Машины");
-        listItem.add("Роботы");
-        listItem.add("Флаги");
-
-        ListView drawerListView = (ListView) findViewById(R.id.left_drawer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-        // подключим адаптер для списка
-        drawerListView.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, R.id.text1, listItem));
-        drawerListView.setOnItemClickListener(new DrawerItemClickListener());
-
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         fragmentManager = getSupportFragmentManager();
 
@@ -71,25 +73,40 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            String tag = listItem.get(position);
-
-            switch (tag) {
-                case "Машины":
-                    tagEng = "car";
-                    break;
-                case "Роботы":
-                    tagEng = "robots";
-                    break;
-                case "Флаги":
-                    tagEng = "flags";
-                    break;
-            }
-
-            viewPager.setAdapter(new FragmentPagerAdapter(fragmentManager, tagEng));
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.cars:
+                tagEng = "car";
+                break;
+            case R.id.robots:
+                tagEng = "robots";
+                break;
+            case R.id.flights:
+                tagEng = "flags";
+                break;
+            case R.id.trains:
+                tagEng = "flags";
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        viewPager.setAdapter(new FragmentPagerAdapter(fragmentManager, tagEng));
+        return true;
     }
 }
