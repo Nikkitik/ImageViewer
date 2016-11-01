3412f81b80888532d5920817b2d7e3decebc8ddb
nikkitik <nikkitik21@gmail.com>
2016-11-01 11:46:20 +0300

    удалены неиспользуемые библиотеки

--- app/src/main/java/com/example/nromantsov/imageviewer/MainActivity.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/MainActivity.java
@@ -12,16 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 import com.example.nromantsov.imageviewer.Fragment.FragmentPagerAdapter;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     FragmentManager fragmentManager;
