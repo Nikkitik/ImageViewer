b5189a0139f519aab5c25a199e35d5f554416034
nikkitik <nikkitik21@gmail.com>
2016-11-02 13:57:09 +0300

    если данные были добавлены в бд то обновляем "Избранное"

--- app/src/main/java/com/example/nromantsov/imageviewer/Fragment/FragmentFavorite.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/Fragment/FragmentFavorite.java
@@ -1,5 +1,6 @@
 package com.example.nromantsov.imageviewer.Fragment;
 
+import android.app.Application;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,40 +12,51 @@ import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.nromantsov.imageviewer.Adapter.ItemClickSupport;
 import com.example.nromantsov.imageviewer.Adapter.RecyclerAdapter;
+import com.example.nromantsov.imageviewer.ApplicationBase;
 import com.example.nromantsov.imageviewer.DataBase.DbHandler;
+import com.example.nromantsov.imageviewer.MainActivity;
 import com.example.nromantsov.imageviewer.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
-public class FragmentFavorite extends Fragment {
+public class FragmentFavorite extends Fragment implements Observer {
 
     RecyclerAdapter adapter = null;
+    RecyclerView recyclerView;
+    DbHandler dbHandler;
+
     List<String> urlBase = new ArrayList<>();
     String tag;
+    int screenWidth, screenHeight;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_favorite, container, false);
 
+        ApplicationBase.obs.getObserverChange().addObserver(this);
+
         Bundle bundle = getArguments();
 
         if (bundle != null)
-            tag = getArguments().getString("tag", "weather");
+            tag = bundle.getString("tag", "weather");
 
-        DbHandler dbHandler = new DbHandler(getActivity());
+        dbHandler = new DbHandler(getActivity());
         urlBase = dbHandler.getUrls(tag);
 
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        int screenWidth = displaymetrics.widthPixels;
-        int screenHeight = displaymetrics.heightPixels;
+        screenWidth = displaymetrics.widthPixels;
+        screenHeight = displaymetrics.heightPixels;
 
-        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.rvImage);
+        recyclerView = (RecyclerView) v.findViewById(R.id.rvImage);
 
         final RecyclerView.LayoutManager layoutManager;
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
@@ -77,5 +89,13 @@ public class FragmentFavorite extends Fragment {
         });
 
         return v;
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        urlBase = dbHandler.getUrls(tag);
+        adapter = new RecyclerAdapter(urlBase, screenHeight, screenWidth);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
