54971486d84bf1a57c352702e9a1146a1a62524c
nikkitik <nikkitik21@gmail.com>
2016-11-02 13:55:27 +0300

    создан ObserverChange

---
+++ app/src/main/java/com/example/nromantsov/imageviewer/ApplicationBase.java
@@ -0,0 +1,28 @@
+package com.example.nromantsov.imageviewer;
+
+import com.example.nromantsov.imageviewer.DataBase.ObserverChange;
+import com.facebook.stetho.Stetho;
+
+public class ApplicationBase extends android.app.Application {
+    public static Obs obs;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+        obs = new Obs();
+    }
+
+    public static class Obs
+    {
+        private  ObserverChange observerChange;
+        public Obs()
+        {
+            observerChange = new ObserverChange();
+        }
+
+        public ObserverChange getObserverChange() {
+            return observerChange;
+        }
+    }
+}
