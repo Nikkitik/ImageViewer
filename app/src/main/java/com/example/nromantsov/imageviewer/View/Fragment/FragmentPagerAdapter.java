21f1d464967b5bee2003fb409f6b2736f30a83b2
nikkitik <nikkitik21@gmail.com>
2016-11-09 17:09:27 +0300

    убран tag

--- app/src/main/java/com/example/nromantsov/imageviewer/View/Fragment/FragmentPagerAdapter.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/View/Fragment/FragmentPagerAdapter.java
@@ -1,19 +1,13 @@
 package com.example.nromantsov.imageviewer.View.Fragment;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
-import com.example.nromantsov.imageviewer.Fragment.FragmentFavorite;
-
 public class FragmentPagerAdapter extends FragmentStatePagerAdapter {
 
-    private String tag;
-
-    public FragmentPagerAdapter(FragmentManager fm, String tag) {
+    public FragmentPagerAdapter(FragmentManager fm) {
         super(fm);
-        this.tag = tag;
     }
 
     @Override
