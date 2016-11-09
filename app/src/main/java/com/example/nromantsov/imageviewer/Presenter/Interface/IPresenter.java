71158a4ad85ba3334ec251e89090c5274819e6b1
nikkitik <nikkitik21@gmail.com>
2016-11-09 14:00:21 +0300

    добавлены страницы

--- app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenter.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenter.java
@@ -2,6 +2,10 @@ package com.example.nromantsov.imageviewer.Presenter.Interface;
 
 public interface IPresenter {
     void getUrl();
+
     void setTag(String tag);
     String getTag();
+
+    void setPage(int page);
+    Integer getPage();
 }
