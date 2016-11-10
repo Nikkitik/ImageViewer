ea02d453e7fe6788f90226a5cfdc61f889fa6dcd
nikkitik <nikkitik21@gmail.com>
2016-11-10 15:41:39 +0300

    commen

--- app/src/main/java/com/example/nromantsov/imageviewer/Presenter/AboutPresenter.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/Presenter/AboutPresenter.java
@@ -1,4 +1,71 @@
 package com.example.nromantsov.imageviewer.Presenter;
 
-public class AboutPresenter {
+import android.app.Activity;
+import android.widget.ImageView;
+
+import com.example.nromantsov.imageviewer.Model.DbHandler;
+import com.example.nromantsov.imageviewer.Model.DownLoadImage;
+import com.example.nromantsov.imageviewer.Model.UrlBase;
+import com.example.nromantsov.imageviewer.Presenter.Interface.IPresenterAbout;
+import com.example.nromantsov.imageviewer.View.Interface.IViewAbout;
+
+import java.util.List;
+
+public class AboutPresenter implements IPresenterAbout {
+    private IViewAbout iViewAbout;
+    private Activity activity;
+    private ImageView imageView;
+
+    private DbHandler dbHandler;
+    private String tag, url;
+    private List<String> urlList;
+    private Boolean flag = false;
+
+    public AboutPresenter(IViewAbout iViewAbout, Activity activity, ImageView imageView) {
+        this.iViewAbout = iViewAbout;
+        this.activity = activity;
+        this.imageView = imageView;
+    }
+
+    @Override
+    public void setUrlTag(String url, String tag) {
+        this.tag = tag;
+        this.url = url;
+        imageView.setTag(url);
+        new DownLoadImage(imageView).execute(url);
+    }
+
+    @Override
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    @Override
+    public List<String> loadUrlsDataBase() {
+        dbHandler = new DbHandler(activity.getApplicationContext());
+        urlList = dbHandler.getUrls(tag);
+
+        for (int i = 0; i < urlList.size(); i++) {
+            if (url.equals(urlList.get(i))) {
+                flag = true;
+                break;
+            }
+            flag = false;
+        }
+        iViewAbout.colorFloatingActionButton(flag);
+
+        return urlList;
+    }
+
+    @Override
+    public void addDataBase() {
+        dbHandler.addUrl(new UrlBase(tag, url));
+        loadUrlsDataBase();
+    }
+
+    @Override
+    public void deleteDataBase() {
+        dbHandler.deleteUrlFavorite(url);
+        loadUrlsDataBase();
+    }
 }
