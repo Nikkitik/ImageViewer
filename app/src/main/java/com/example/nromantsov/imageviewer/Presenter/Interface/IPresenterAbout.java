74b88f11f835529c8321de705288197d1c79200a
nikkitik <nikkitik21@gmail.com>
2016-11-10 17:10:11 +0300

    показать, отменить snackbar

--- app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenterAbout.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenterAbout.java
@@ -1,11 +1,15 @@
 package com.example.nromantsov.imageviewer.Presenter.Interface;
 
+import android.view.View;
+
 import java.util.List;
 
 public interface IPresenterAbout {
     void setUrlTag (String url, String tag);
-    Boolean getFlag();
     List<String> loadUrlsDataBase();
+
+    void applySnackBar(View view);
+    void cancelSnackBar();
 
     void addDataBase();
     void deleteDataBase();
