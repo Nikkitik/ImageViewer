7b805c88452ab485a54dc4918e3e65ac9a81cb07
nikkitik <nikkitik21@gmail.com>
2016-11-10 13:02:45 +0300

    загрузка FragmentAbout

--- app/src/main/java/com/example/nromantsov/imageviewer/View/Interface/IViewMain.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/View/Interface/IViewMain.java
@@ -2,8 +2,10 @@ package com.example.nromantsov.imageviewer.View.Interface;
 
 import java.util.List;
 
-public interface IView {
+public interface IViewMain {
     void loadUrl(List<String> urls);
+
+    void loadAboutFragment(String url, String tag);
 
     void showProgressbar();
     void hideProgressbar();
