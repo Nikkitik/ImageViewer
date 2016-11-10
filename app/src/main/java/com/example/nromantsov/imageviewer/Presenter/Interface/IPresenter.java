11bf4a2330916cb36ae2f4ca18bd7ca17b89ee44
nikkitik <nikkitik21@gmail.com>
2016-11-10 13:02:59 +0300

    setUrlAbout

--- app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenter.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenter.java
@@ -2,6 +2,7 @@ package com.example.nromantsov.imageviewer.Presenter.Interface;
 
 public interface IPresenter {
     void getUrl();
+    void setUrlAbout(String url);
 
     void setTag(String tag);
     String getTag();
