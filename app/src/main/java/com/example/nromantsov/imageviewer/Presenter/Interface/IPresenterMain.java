a778a1f5db551cdf3d1c3069f65b84041bc09518
nikkitik <nikkitik21@gmail.com>
2016-11-11 17:22:09 +0300

    получение fragment name

--- app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenterMain.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenterMain.java
@@ -1,8 +1,14 @@
 package com.example.nromantsov.imageviewer.Presenter.Interface;
 
+import java.util.List;
+
 public interface IPresenterMain {
     void getUrl();
     void setUrlAbout(String url);
+    void setUrlFavorite(List<String> urls);
+
+    void setFragmentName(String name);
+    String getFragmentName();
 
     void setTag(String tag);
     String getTag();
