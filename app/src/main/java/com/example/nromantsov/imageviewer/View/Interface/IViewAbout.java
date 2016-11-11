374ea071cfa6288a4e771ae5c70c9c56f037f662
nikkitik <nikkitik21@gmail.com>
2016-11-11 11:45:34 +0300

    загрузка картинки

--- app/src/main/java/com/example/nromantsov/imageviewer/View/Interface/IViewAbout.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/View/Interface/IViewAbout.java
@@ -1,10 +1,12 @@
 package com.example.nromantsov.imageviewer.View.Interface;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.View;
 
 public interface IViewAbout {
     void colorFloatingActionButton(Boolean result);
     void showSnackBar(View view, String msg);
     Context getContext();
+    void loadImage(Bitmap bitmap);
 }
