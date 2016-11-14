e616d2fd7b33ef5d92f39c761c46473358fc367a
nikkitik <nikkitik21@gmail.com>
2016-11-14 13:55:45 +0300

    loadFragmentFavorite

--- app/src/main/java/com/example/nromantsov/imageviewer/View/Interface/IViewMainActivity.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/View/Interface/IViewMainActivity.java
@@ -3,4 +3,6 @@ package com.example.nromantsov.imageviewer.View.Interface;
 public interface IViewMainActivity {
     void removeAbout();
     void onTag(String tag);
+
+    void loadFragmentFavorite();
 }
