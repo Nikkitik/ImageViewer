ba80e62a8c2398f01ebbf242e908b4bcf145f3ce
nikkitik <nikkitik21@gmail.com>
2016-11-14 13:55:29 +0300

    delete all favorite

--- app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenterMainActivity.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenterMainActivity.java
@@ -3,7 +3,9 @@ package com.example.nromantsov.imageviewer.Presenter.Interface;
 public interface IPresenterMainActivity {
     void deleteFileFromFolder();
     void deleteFileFromDataBase(String tag);
+    void deleteAllFileFromDataBase();
     void createDialogSearch();
+    void createFavoriteAll();
 
     void setTagDialogSearch(String tag);
 }
