05c18b08ff7ae8035f2febb33f417efecf0ad276
nikkitik <nikkitik21@gmail.com>
2016-11-15 10:33:13 +0300

    получение списка tag для favoriteAll определение tag по позиции

--- app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenterMain.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/Presenter/Interface/IPresenterMain.java
@@ -6,12 +6,14 @@ public interface IPresenterMain {
     void getUrl();
     void setUrlAbout(String url);
     void setUrlFavorite(List<String> urls);
+    void setTagFavorite(List<String> tags);
 
     void setFragmentName(String name);
     String getFragmentName();
 
     void setTag(String tag);
     String getTag();
+    String getTagFavorite(int position);
 
     void setPage(int page);
     Integer getPage();
