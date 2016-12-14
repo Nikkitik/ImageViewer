98935e91cfa7a2496b7e48f60f9a9fa17d583fa7
nikkitik <nikkitik21@gmail.com>
2016-12-14 17:35:45 +0300

    rename

--- app/src/main/java/com/example/nromantsov/imageviewer/Model/Interface/IModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.nromantsov.imageviewer.Model.Interface;
-
-import java.util.List;
-
-public interface IModel {
-    void listUrl(List<String> urlList);
-}
--- app/src/main/java/com/example/nromantsov/imageviewer/View/Interface/IViewMainActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.nromantsov.imageviewer.View.Interface;
-
-public interface IViewMainActivity {
-    void removeAbout();
-    void onTag(String tag);
-
-    void loadFragmentFavorite();
-}
