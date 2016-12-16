437f839a4bf9cd381b35a9e69bf10f237a3131fd
nikkitik <nikkitik21@gmail.com>
2016-12-16 12:33:37 +0300

    создан новый класс JSONUtils

--- app/src/main/java/com/example/nromantsov/imageviewer/Model/ParserJSON.java
+++ app/src/main/java/com/example/nromantsov/imageviewer/Model/ParserJSON.java
@@ -1,19 +1,14 @@
 package com.example.nromantsov.imageviewer.model;
 
 import android.os.AsyncTask;
+
 import com.example.nromantsov.imageviewer.model.interfaces.IModel;
 import com.example.nromantsov.imageviewer.presenter.interfaces.IPresenterMain;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.nromantsov.imageviewer.model.JSONUtils.connectionURL;
 
 public class ParserJSON extends AsyncTask<String, Integer, Boolean> {
     private List<String> sourceArray = new ArrayList<>();
@@ -28,50 +23,16 @@ public class ParserJSON extends AsyncTask<String, Integer, Boolean> {
 
     @Override
     protected Boolean doInBackground(String... params) {
-        try {
-            URL url = new URL("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4fe038fa52c229b33d82da3283567c6c&tags=" + iPresenter.getTag() + "&license=&page=" + iPresenter.getPage() + "&format=json&nojsoncallback=1");
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuilder buffer = new StringBuilder();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                buffer.append(line);
-            }
-
-            String resultJSON = buffer.toString();
-
-            JSONObject jsonObject = new JSONObject(resultJSON);
-            JSONObject photos = jsonObject.getJSONObject("photos");
-
-            JSONArray photoArray = photos.getJSONArray("photo");
-            String idName;
-            String serverName;
-            String secretName;
-            for (int i = 0; i < photoArray.length(); i++) {
-                JSONObject id = photoArray.getJSONObject(i);
-                idName = id.getString("id");
-                serverName = id.getString("server");
-                secretName = id.getString("secret");
-
-                String source = "https://farm6.staticflickr.com/" + serverName + "/" + idName + "_" + secretName + "_b.jpg";
-                sourceArray.add(source);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        sourceArray = JSONUtils.getSourceArray(connectionURL(iPresenter.getTag(), iPresenter.getPage()));
         return true;
     }
 
     @Override
     protected void onPostExecute(Boolean result) {
         super.onPostExecute(result);
-        if (result)
-            iModel.listUrl(sourceArray);
+        if (!isCancelled()) {
+            if (result)
+                iModel.listUrl(sourceArray);
+        }
     }
 }
